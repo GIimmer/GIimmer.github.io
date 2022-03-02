4bc3c79fa4e6aa5fe2d328acdc1fe1aeaef9c2cb
Riley <rwaugh52@gmail.com>
2022-03-01 23:11:04 -0800

    Cleaning up popoutWindow

--- projects/popout-window/src/lib/popout-window.component.ts
+++ projects/popout-window/src/lib/popout-window.component.ts
@@ -1,9 +1,14 @@
 import {
+  Input,
+  Output,
+  OnDestroy,
   Component,
+  ViewChild,
+  Renderer2,
   ElementRef,
   HostListener,
-  Input, EventEmitter, OnDestroy, Output, Renderer2,
-  ViewChild
+  EventEmitter,  
+  ChangeDetectorRef
 } from '@angular/core';
 
 @Component({
@@ -14,9 +19,11 @@ import {
 
 export class PopoutWindowComponent implements OnDestroy  {
   @Input() windowTitle: string;
+  @Input() whiteIcon: boolean;
   @Output() closed: EventEmitter<boolean> = new EventEmitter();
 
   @ViewChild('innerWrapper') private innerWrapper: ElementRef;
+  @ViewChild('popoutWrapper') private popoutWrapper: ElementRef;
 
   private popoutWindow: Window;
   private observer: MutationObserver;
@@ -26,7 +33,7 @@ export class PopoutWindowComponent implements OnDestroy  {
     this.close();
   }
 
-  constructor(private renderer2: Renderer2, private elementRef: ElementRef) {}
+  constructor(private renderer2: Renderer2, private cd: ChangeDetectorRef) {}
 
   get isPoppedOut() {
     return !!this.popoutWindow;
@@ -37,28 +44,31 @@ export class PopoutWindowComponent implements OnDestroy  {
     this.close();
   }
 
-  private close(): void {
-    if (this.popoutWindow) {
-      this.popoutWindow.close();
-      this.popoutWindow = null;
-      this.closed.next(true)
+  popIn(): void {
+    this.renderer2.appendChild(this.popoutWrapper.nativeElement, this.innerWrapper.nativeElement);
+    this.close();
+    this.cd.detectChanges();
+  }
+
+  popOut(): void {
+    if (!this.popoutWindow) {
+      this.createPopoutWindow();
+      this.cloneStylesToPopoutWindow();
+
+      this.renderer2.appendChild(this.popoutWindow.document.body, this.innerWrapper.nativeElement);
+
+      this.popoutWindow.addEventListener('unload', () => this.popIn());
+
+      this.cd.detectChanges();
+    } else {
+      this.popoutWindow.focus();
     }
   }
-
-  public popIn(): void {
-    this.renderer2.appendChild(this.elementRef.nativeElement, this.innerWrapper.nativeElement);
-    this.close();
-  }
-
-  public popOut(): void {
-    if (!this.popoutWindow) {
+  
+  private createPopoutWindow() {
       const elmRect = this.innerWrapper.nativeElement.getBoundingClientRect();
 
-      const navHeight = window.outerHeight - window.innerHeight;
-      const navWidth = window.outerWidth - window.innerWidth;
-
-      const winLeft = window.screenX + navWidth + elmRect.left;
-      const winTop = window.screenY + navHeight + elmRect.top - 60;
+      const [winLeft, winTop] = this.getWindowPositioning(elmRect);
 
       this.popoutWindow = window.open(
         '',
@@ -71,34 +81,40 @@ export class PopoutWindowComponent implements OnDestroy  {
 
       this.popoutWindow.document.title = this.windowTitle || window.document.title;
       this.popoutWindow.document.body.style.margin = '0';
+  }
 
-      this.cloneStyles();
+  private getWindowPositioning(elmRect: DOMRect) {
+      const navHeight = window.outerHeight - window.innerHeight;
+      const navWidth = window.outerWidth - window.innerWidth;
 
-      this.renderer2.appendChild(this.popoutWindow.document.body, this.innerWrapper.nativeElement);
+      const winLeft = window.screenX + navWidth + elmRect.left;
+      const winTop = window.screenY + navHeight + elmRect.top - 60;
+      return [winLeft, winTop];
+  }
 
-      this.popoutWindow.addEventListener('unload', () => {
-        this.popIn();
-      });
-    } else {
-      this.popoutWindow.focus();
+  private close(): void {
+    if (this.popoutWindow) {
+      this.popoutWindow.close();
+      this.popoutWindow = null;
+      this.closed.next(true)
     }
   }
   
-  private cloneStyles() {
-    document.head.querySelectorAll('style').forEach(node => {
-      this.popoutWindow.document.head.appendChild(node.cloneNode(true));
+  private cloneStylesToPopoutWindow() {
+    document.fonts.forEach(node => {
+      (this.popoutWindow.document as any).fonts.add(node);
     });
-
-    this.observeStyleChanges();
 
     document.head.querySelectorAll('link[rel="stylesheet"]').forEach(node => {
       this.popoutWindow.document.head.insertAdjacentHTML('beforeend',
         `<link rel="stylesheet" type="${(node as HTMLLinkElement).type}" href="${(node as HTMLLinkElement).href}">`);
     });
 
-    (document as any).fonts.forEach(node => {
-      (this.popoutWindow.document as any).fonts.add(node);
+    document.head.querySelectorAll('style').forEach(node => {
+      this.popoutWindow.document.head.appendChild(node.cloneNode(true));
     });
+
+    this.observeStyleChanges();
   }
 
   private observeStyleChanges() {
