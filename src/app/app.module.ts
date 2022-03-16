6199b9de7f68ad1f4deb7a69da5c6775d0a109b8
Riley <rwaugh52@gmail.com>
2022-03-16 00:42:49 -0700

    Updating demo application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MainComponent } from './main/main.component';
-import {FormsModule} from '@angular/forms';
-import { NgStartComponent } from './ng-start/ng-start.component';
-import {PopoutWindowModule} from '../../projects/popout-window/src/lib/popout-window.module';
-import {DragDropModule} from '@angular/cdk/drag-drop';
+import { ReactiveFormsModule } from '@angular/forms';
+import { PopoutWindowModule } from '../../projects/popout-window/src/lib/popout-window.module';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
 
 @NgModule({
   declarations: [
     MainComponent,
-    NgStartComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    ReactiveFormsModule,
     PopoutWindowModule,
     DragDropModule,
+    MatIconModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [MainComponent]
