d97fb158f3ddcc2f511bef5a5825437f8b6842af
Riley <rwaugh52@gmail.com>
2022-03-21 00:12:06 -0700

    Adding popout-window with ng library command

---
+++ projects/angular-opinionated-popout-window/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of angular-opinionated-popout-window
+ */
+
+export * from './lib/popout-window.component';
+export * from './lib/popout-window.module';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MainComponent } from './main/main.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { PopoutWindowModule } from '../../projects/popout-window/src/lib/popout-window.module';
+import { PopoutWindowModule } from '../../projects/angular-opinionated-popout-window/src/lib/popout-window.module';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
